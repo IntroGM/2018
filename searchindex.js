09a1204c452668a89711e2447e80d23158285e30
Dave Whipp <david.whipp@helsinki.fi>
2018-05-08 15:29:39 +0300

    Generated gh-pages for commit 741a308 Author: Dave Whipp <david.whipp@helsinki.fi>
        Added build files for gh-pages deployment

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to the Introduction to Geodynamic Modelling course!"],titleterms:{cours:0,geodynam:0,indic:0,introduct:0,model:0,tabl:0,welcom:0}})
